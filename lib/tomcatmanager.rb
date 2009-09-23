3cc6cfe1578c7e4eae7fe3893abe850d8e4ccb00
Hayato <haya10.ito+github@gmail.com>
2009-09-23 13:20:31 +0900

    add sources

---
+++ lib/tomcatmanager.rb
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+require 'net/http'
+Net::HTTP.version_1_2
+
+
+class TomcatManager
+
+  class TomcatManager::Unauthorized < Exception; end
+  class TomcatManager::WarNotFound < Exception
+    def initlalized(filename)
+      super("war file not found : #{filename}")
+    end
+  end
+
+  attr_accessor :manager
+  attr_accessor :password
+  attr_accessor :host
+  attr_accessor :port
+
+  def initialize 
+    @host = "127.0.0.1"
+    @port = "8080"
+    yield self if block_given?
+  end
+  
+  def list
+    result = []
+    request('/manager/list').body.each_line do |line|
+      next if line =~ /^OK/
+      e = line.split(/:/)
+      result << { 
+        :path => e[0],
+        :status => e[1],
+        :session => e[2],
+        :name => e[3],
+      }
+    end
+    
+    result
+  end
+
+  def undeploy path
+    response = request("/manager/undeploy?path=/#{path}")
+    puts response.body
+  end
+
+  def deploy filename, path=nil
+    raise WarNotFound.new(filename) unless File.exists?(filename)
+
+    def default_pathname filename
+      result =File.basename(filename).gsub( /\.war/i , "")
+      puts result
+      result
+    end
+    path = default_pathname(filename) if path.nil?
+
+    response = request("/manager/deploy?path=/#{path}",:put) do |req|
+      File.open(filename){ |f| req.body = f.read }
+    end
+
+    puts response.body
+  end
+
+  def request(path,method=:get)
+    Net::HTTP.start(@host,@port) do |http|
+      case method
+      when :get
+        req = Net::HTTP::Get.new(path)
+      when :put
+        req = Net::HTTP::Put.new(path)
+      else
+        raise ArgumentError.new("invalid method #{method}")
+      end
+
+      req.basic_auth @manager, @password
+
+      yield req if block_given?
+
+      response = http.request(req)
+      raise TomcatManager::Unauthorized if response.code == "401"
+      
+      return response
+    end
+  end
+
+end
--- spec/tomcatmanager_spec.rb
+++ spec/tomcatmanager_spec.rb
@@ -1,7 +1,83 @@
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'fakeweb'
 
 describe "Tomcatmanager" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  
+
+  before(:each) do
+    @account = "manager"
+    @incorrect_account = "incorrect"
+    @password = "password"
+    @host = "127.0.0.1"
+    @port = "8080"
+
+    FakeWeb.register_uri(:get, "http://#{@incorrect_account}:#{@password}@#{@host}:#{@port}/manager/list", :body => "Unauthorized", :status => ["401", "Unauthorized"])
+
+    FakeWeb.register_uri(:get, "http://#{@account}:#{@password}@#{@host}:#{@port}/manager/list",
+                         :body => <<END_OF_RESPONSE
+OK - バーチャルホスト localhost のアプリケーション一覧です
+/:running:0:ROOT
+/manager:running:3:manager
+/hudson:running:0:hudson
+/apache-solr-nightly:running:0:apache-solr-nightly
+/docs:running:0:docs
+/examples:running:0:examples
+/host-manager:running:0:host-manager
+/solr-search-example:running:0:solr-search-example
+END_OF_RESPONSE
+                         )
+
   end
+
+  after(:each) do
+    FakeWeb.clean_registry
+  end
+
+  it "has account info" do
+    tm = TomcatManager.new do |tm|
+      tm.manager="manager"
+      tm.password="password"
+    end
+    tm.manager.should == "manager"
+    tm.password.should == "password"
+  end
+
+  it "has list info after called list method accessed by correct password" do
+
+    tomcat = TomcatManager.new do |tm|
+      tm.manager=@account
+      tm.password=@password
+    end
+
+    list = tomcat.list
+
+    list.first[:path].should == "/"
+    list.size.should == 8
+  end
+  
+  it "throw unauthorized exception accessed by incorrect password" do
+
+    tomcat = TomcatManager.new do |tm|
+      tm.manager=@incorrect_account
+      tm.password=@password
+    end
+
+    lambda{tomcat.list}.should raise_error(TomcatManager::Unauthorized)
+
+  end
+
+  it "throw FileNotFound at deploy method with not exist war" do
+    tomcat = TomcatManager.new do |tm|
+      tm.manager=@account
+      tm.password=@password
+    end
+    
+    lambda{ tomcat.deploy "/tmp/aaaaaaaa.war","testpath" }.should raise_error(TomcatManager::WarNotFound)
+  end
+
+  it "throw ArgumentError by request method without :get or :put" do
+    lambda{ TomcatManager.new.request("/test",:invalid_method) }.should raise_error(ArgumentError)
+  end
+
 end
